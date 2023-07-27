57d7997589ea3ef6e538aa97bbe67d66a0b1126e
ncinco <32296929+ncinco@users.noreply.github.com>
2023-07-27 13:50:30 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "1.50.0"
     }
   }
+
+backend "azurerm" {
+    resource_group_name  = "Terraform_State"
+    storage_account_name = "confluentstate"
+    container_name       = "dev"
+    key                  = "confluentstate.tfstate"
+  }
 }
 
 provider "confluent" {
