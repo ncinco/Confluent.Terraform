2d7db0a742645248ec84a11f267fcc37ed12f7ef
ncinco <32296929+ncinco@users.noreply.github.com>
2023-07-27 14:01:16 +1200

    Update serviceaccounts.tf

--- serviceaccounts.tf
+++ serviceaccounts.tf
@@ -2,3 +2,12 @@ resource "confluent_service_account" "basic_kafka_cluster-app-manager" {
   display_name = "Application Manager"
   description  = "Service account to manage 'basic_kafka_cluster' Kafka cluster"
 }
+
+resource "confluent_api_key" "basic_kafka_cluster-app-manage-kafka-api-key" {
+  display_name = "basic_kafka_cluster-app-manage-kafka-api-key"
+  description  = "Kafka API Key that is owned by 'basic_kafka_cluster-app-manage' service account"
+  owner {
+    id          = confluent_service_account.basic_kafka_cluster-app-manager.id
+    api_version = confluent_service_account.basic_kafka_cluster-app-manager.api_version
+    kind        = confluent_service_account.basic_kafka_cluster-app-manager.kind
+  }
