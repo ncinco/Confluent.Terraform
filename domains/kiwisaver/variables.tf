c6a137eafc1a3b9964c9b12a53443adf93d08df0
ncinco <ncinco@gmail.com>
2023-08-04 09:38:22 +1200

    use of tfvars

--- domains/kiwisaver/variables.tf
+++ domains/kiwisaver/variables.tf
@@ -2,25 +2,21 @@
 variable "cluster_name" {
   description = "Name of a cluster"
   type        = string
-  default     = "kiwisaver_cluster"
 }
 
 variable "environment" {
   description = "Name of environment"
   type        = string
-  default     = "kiwisaver_prod"
 }
 
 variable "cluster_cloud" {
   description = "Cloud provider of a cluster"
   type        = string
-  default     = "AZURE"
 }
 
 variable "cluster_region" {
   description = "Region of a cluster"
   type        = string
-  default     = "australiaeast"
 }
 
 # topic related variables
@@ -31,9 +27,4 @@ variable "topics" {
     partitions_count = number
     cleanup_policy = string
   }))
-  default = [
-    { name = "growth", partitions_count = 8, cleanup_policy = "delete"},
-    { name = "conservative", partitions_count = 7, cleanup_policy = "delete"},
-    { name = "batman", partitions_count = 7, cleanup_policy = "delete"}
-  ]
 }
