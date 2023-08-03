a2afb7837b9cdf5e3343cbee648f983b795c1294
ncinco <ncinco@gmail.com>
2023-08-03 13:34:30 +1200

    remove tpl, use variable

---
+++ accounts/variables.tf
@@ -0,0 +1,31 @@
+variable "cluster_name" {
+  description = "Name of a cluster"
+  type        = string
+  default     = "bank_accounts_cluster"
+}
+
+variable "cluster_cloud" {
+  description = "Cloud provider of a cluster"
+  type        = string
+  default     = "AZURE"
+}
+
+variable "cluster_region" {
+  description = "Region of a cluster"
+  type        = string
+  default     = "australiaeast"
+}
+
+variable "topics" {
+  description = "List of kafka topic(s) for a cluster"
+  type = list(object({
+    name = string
+    partitions_count = number
+    cleanup_policy = string
+  }))
+  default = [
+    { name = "accounts", partitions_count = 8, cleanup_policy = "delete"},
+    { name = "transactions", partitions_count = 7, cleanup_policy = "delete"},
+    { name = "superman", partitions_count = 7, cleanup_policy = "delete"}
+  ]
+}
