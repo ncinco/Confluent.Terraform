5fff44a5e7e283ca39b0e431d38ebd8ca213193f
ncinco <ncinco@gmail.com>
2023-08-07 12:09:28 +1200

    remove variables

--- modules/controlplane/variables.tf
+++ modules/controlplane/variables.tf
@@ -1,4 +1,4 @@
-variable "confluent_cloud_api_key" {
+/* variable "confluent_cloud_api_key" {
   description = "Confluent Cloud API Key (also referred as Cloud API ID)"
   type        = string
 }
@@ -7,7 +7,7 @@ variable "confluent_cloud_api_secret" {
   description = "Confluent Cloud API Secret"
   type        = string
   sensitive   = true
-}
+} */
 
 variable "environment" {
   description = "Name of environment"
