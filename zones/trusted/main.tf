acdbcc19a205668d400d16a1339223b57404d721
ncinco <ncinco@gmail.com>
2023-08-08 16:08:37 +1200

    az missed

--- zones/trusted/main.tf
+++ zones/trusted/main.tf
@@ -4,6 +4,10 @@ terraform {
       source = "confluentinc/confluent"
       version = "1.51.0"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.57.0"
+    }
   }
 
   backend "azurerm" {
