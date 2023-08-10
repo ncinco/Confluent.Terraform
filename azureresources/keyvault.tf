f8ca2d996d57213898248ed032202bb6fd9b83ac
ncinco <32296929+ncinco@users.noreply.github.com>
2023-08-10 22:32:17 +1200

    Update keyvault.tf

--- azureresources/keyvault.tf
+++ azureresources/keyvault.tf
@@ -26,3 +26,9 @@ resource "azurerm_key_vault" "confluent_cloud_key_vault" {
     ]
   }
 }
+
+resource "azurerm_role_assignment" "key_vault_role_assingment" {
+  scope                = azurerm_key_vault.confluent_cloud_key_vault.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
