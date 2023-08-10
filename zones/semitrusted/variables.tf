b40fea1b93a80199524d074b0a836f062cef8d0d
ncinco <32296929+ncinco@users.noreply.github.com>
2023-08-10 16:17:12 +1200

    Update variables.tf

--- zones/semitrusted/variables.tf
+++ zones/semitrusted/variables.tf
@@ -18,3 +18,52 @@ variable "cluster_region" {
   description = "Region of a cluster"
   type        = string
 }
+
+# service account related variables
+variable "service_accounts" {
+  description = "List of service accounts(s) for a cluster"
+  type = list(object({
+    name = string
+    description = string
+  }))
+}
+
+variable "api_keys" {
+  description = "List of api key(s) for a cluster"
+  type = list(object({
+    name = string
+    description = string
+    service_account_id = string
+    cluster_id = string
+  }))
+}
+
+
+
+
+
+# azure resources
+variable "resource_group_name" {
+  description = "Name of resource group"
+  type        = string
+}
+
+variable "resource_group_location" {
+  description = "Location of resource group"
+  type        = string
+}
+
+variable "key_vault_name" {
+  description = "Key Vault name"
+  type        = string
+}
+
+variable "key_vault_sku_name" {
+  description = "SKU name"
+  type        = string
+}
+
+variable "key_vault_soft_delete_retention_days" {
+  description = "Soft delete retention days."
+  type        = string
+}
