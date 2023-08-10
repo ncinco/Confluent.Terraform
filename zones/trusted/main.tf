260124f4665212d4ce35268c8a83ad3e7b4be9e8
ncinco <32296929+ncinco@users.noreply.github.com>
2023-08-10 15:34:57 +1200

    Update main.tf

--- zones/trusted/main.tf
+++ zones/trusted/main.tf
@@ -15,7 +15,9 @@ terraform {
     storage_account_name = "confluentstate"
     container_name       = "trusted"
     key                  = "control_plane.tfstate"
-    use_azuread_auth     = true
+    use_msi              = true
+    subscription_id      = "7e36d9ff-064f-4f22-bcee-0a9c849d48f5"
+    tenant_id            = "2cad9261-8e15-4a74-9a69-65f0713c088e"
   }
 }
 
