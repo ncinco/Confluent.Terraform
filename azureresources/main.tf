18e32fdff1770e0d1c74cfc52c594d687428e881
ncinco <32296929+ncinco@users.noreply.github.com>
2023-08-10 22:12:35 +1200

    Update main.tf

--- azureresources/main.tf
+++ azureresources/main.tf
@@ -21,15 +21,15 @@ provider "azurerm" {
 data "azurerm_client_config" "current" {
 }
 
-resource "azurerm_resource_group" "confluent_state" {
+resource "azurerm_resource_group" "confluent_state_resource_group" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
 
 resource "azurerm_key_vault" "confluent_cloud_key_vault" {
   name                        = var.key_vault_name
-  location                    = var.resource_group_location
-  resource_group_name         = var.resource_group_name
+  location                    = azurerm_resource_group.confluent_state_resource_group.location
+  resource_group_name         = azurerm_resource_group.confluent_state_resource_group.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = var.key_vault_soft_delete_retention_days
