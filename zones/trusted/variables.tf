30b92c04d5aba36d8dd210b9cffc343ea1ad3d77
ncinco <32296929+ncinco@users.noreply.github.com>
2023-08-10 23:03:59 +1200

    Update variables.tf

--- zones/trusted/variables.tf
+++ zones/trusted/variables.tf
@@ -37,33 +37,3 @@ variable "api_keys" {
     cluster_id = string
   }))
 }
-
-
-
-
-
-# azure resources
-variable "resource_group_name" {
-  description = "Name of resource group"
-  type        = string
-}
-
-variable "resource_group_location" {
-  description = "Location of resource group"
-  type        = string
-}
-
-variable "key_vault_name" {
-  description = "Key Vault name"
-  type        = string
-}
-
-variable "key_vault_sku_name" {
-  description = "SKU name"
-  type        = string
-}
-
-variable "key_vault_soft_delete_retention_days" {
-  description = "Soft delete retention days."
-  type        = string
-}
