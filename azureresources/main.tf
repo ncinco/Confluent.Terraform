da1cf5465b41485be6d4962f61aa53be0f8b18ff
ncinco <ncinco@gmail.com>
2023-08-10 16:47:23 +1200

    .

--- azureresources/main.tf
+++ azureresources/main.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.57.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "Terraform_State"
+    storage_account_name = "confluentstate"
+    container_name       = "azureresources"
+    key                  = "azureresources.tfstate"
+  }
 }
 
 provider "azurerm" {
