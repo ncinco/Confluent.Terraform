795e776b1572039905bf5098ebbba83e0a5541d9
ncinco <32296929+ncinco@users.noreply.github.com>
2023-08-10 22:31:51 +1200

    Update resourcegroup.tf

--- azureresources/resourcegroup.tf
+++ azureresources/resourcegroup.tf
@@ -2,3 +2,9 @@ resource "azurerm_resource_group" "confluent_state_resource_group" {
   name     = var.resource_group_name
   location = var.resource_group_location
 }
+
+resource "azurerm_role_assignment" "resource_group_role_assingment" {
+  scope                = azurerm_resource_group.confluent_state_resource_group.id
+  role_definition_name = "Owner"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
