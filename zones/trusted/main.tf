9688c244a72027e5f23ef8096b7dd303418cb63d
ncinco <32296929+ncinco@users.noreply.github.com>
2023-08-11 13:53:37 +1200

    Update main.tf

--- zones/trusted/main.tf
+++ zones/trusted/main.tf
@@ -40,7 +40,7 @@ module "controlplane" {
 
   # service principal related variables
   azure_subscription_id      = var.azure_subscription_id
-  azure_tenant_id            = var.azure_tenant_id
-  azure_client_id            = var.azure_client_id
-  azure_client_secret        = var.azure_client_secret
+  AZURE_TENANT_ID            = var.azure_tenant_id
+  AZURE_CLIENT_ID            = var.azure_client_id
+  AZURE_CLIENT_SECRET        = var.azure_client_secret
 }
