2be4269409edacf2cc0d9cd22cf37c2799104865
ncinco <ncinco@gmail.com>
2023-08-11 12:08:15 +1200

    encoding

--- modules/controlplane/createApiKey.py
+++ modules/controlplane/createApiKey.py
@@ -3,7 +3,17 @@ import pip._vendor.requests
 import json
 import os
 
-# Set variables passed from Terraform and other variables.
+# functions
+def encode_token(key: str, secret: str):
+  key_and_secret=f"{app_manager_api_key}:{app_manager_api_secret}"
+  key_and_secret_bytes = key_and_secret.encode("ascii")
+  base64_bytes = base64.b64encode(key_and_secret_bytes)
+  return base64_bytes.decode('ascii')
+
+# constants
+confluent_cloud_api_url = "https://api.confluent.cloud/iam/v2/api-keys"
+
+# variables
 app_manager_api_key = os.environ["app_manager_api_key"]
 app_manager_api_secret = os.environ["app_manager_api_secret"]
 
@@ -12,9 +22,8 @@ description = os.environ["description"]
 owner_id = os.environ["owner_id"]
 resource_id = os.environ["resource_id"]
 
-access_token_bytes = base64.encode(f"{app_manager_api_key}:{app_manager_api_secret}")
-access_token_encode_bytes = base64.b64encode(access_token_bytes)
-access_token = access_token_encode_bytes.decode("ascii")
+# main body
+access_token = encode_token(app_manager_api_key, app_manager_api_secret)
 
 print(f"key and secret: {app_manager_api_key} + {app_manager_api_secret}")
 print(f"access_token: {access_token}")
@@ -22,9 +31,6 @@ print(f"display_name: {display_name}")
 print(f"description: {description}")
 print(f"owner_id: {owner_id}")
 print(f"resource_id: {resource_id}")
-
-# constants
-confluent_cloud_api_url = "https://api.confluent.cloud/iam/v2/api-keys"
 
 # Common http headers
 headers = {
