7e9b7ebfcbc428dab13cc441b41ef5730071d4aa
ncinco <32296929+ncinco@users.noreply.github.com>
2023-08-11 14:52:51 +1200

    Update main.tf

--- modules/controlplane/main.tf
+++ modules/controlplane/main.tf
@@ -30,10 +30,10 @@ resource "terraform_data" "confluent_api_key" {
       resource_id               = each.value.cluster_id
 
       # service principal related variables
-      AZURE_SUBSCRIPTION_ID      = var.azure_subscription_id
-      AZURE_TENANT_ID            = var.azure_client_id
-      AZURE_CLIENT_ID            = var.azure_tenant_id
-      AZURE_CLIENT_SECRET        = var.azure_client_secret
+      azure_subscription_id      = var.azure_subscription_id
+      azure_client_id            = var.azure_client_id
+      azure_tenant_id            = var.azure_tenant_id
+      azure_client_secret        = var.azure_client_secret
 
       // make sure we do not print the token in terraform command prompt output
       quiet = true
