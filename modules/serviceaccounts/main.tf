3d77397f87057fb81de940e252e5e2ad47eb1a1b
ncinco <ncinco@gmail.com>
2023-08-14 14:11:43 +1200

    var

--- modules/serviceaccounts/main.tf
+++ modules/serviceaccounts/main.tf
@@ -54,7 +54,7 @@ resource "confluent_kafka_acl" "acls" {
 
   resource_type = each.value.resource_type
   resource_name = each.value.resource_name
-  pattern_type  = each.value.pattern_type
+  pattern_type  = "LITERAL"
   principal     = each.value.principal
   host          = each.value.host
   operation     = each.value.operation
