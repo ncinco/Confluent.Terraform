b81ac1de88ef7b1ea02daac71cbdf427cb812c01
ncinco <ncinco@gmail.com>
2023-08-18 10:11:14 +1200

    schema avro

--- schemas/trusted/main.tf
+++ schemas/trusted/main.tf
@@ -28,3 +28,18 @@ module "schemas" {
   registry_region = var.registry_region
 }
 
+
+resource "confluent_schema" "purchase" {
+  schema_registry_cluster {
+    id = "lsrc-kgz6m2"
+  }
+  rest_endpoint = "https://psrc-9wjxm.southeastasia.azure.confluent.cloud"
+  # https://developer.confluent.io/learn-kafka/schema-registry/schema-subjects/#topicnamestrategy
+  subject_name = "purchase-topic-value"
+  format = "AVRO"
+  schema = file("./schemas/avro/purchase.avsc")
+  credentials {
+    key    = "YINFU7VOI67X56JX"
+    secret = "ewDBIIQyb91ZrGCpsKsolC2vlnid2/BYYDXg09npgAt5Qlz/wTVeRo1FA0whMT6i"
+  }
+}
